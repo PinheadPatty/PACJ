b1b63064190cdbcaadfb121299213a63ceb67af2
Patrick Lucas <pllucas@andrew.cmu.edu>
2026-04-20 15:09:28 -0400

    fixing namespacing issues for rviz -- pretty important

--- pacj_ws/src/pacj/launch/laptop.launch.py
+++ pacj_ws/src/pacj/launch/laptop.launch.py
@@ -9,6 +9,7 @@ def generate_launch_description():
     rtabmap_pkg = get_package_share_directory('rtabmap_launch')
     rtabmap_launch_path = os.path.join(rtabmap_pkg, 'launch', 'rtabmap.launch.py')
 
+    # --- YOUR ORIGINAL NODES (restored) ---
     vio_relay = Node(
         package='pacj',
         executable='vio_relay',
@@ -37,18 +38,20 @@ def generate_launch_description():
         output='screen'
     )
 
+    # --- SLAM ---
     drone_slam = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(rtabmap_launch_path),
         launch_arguments={
             'rgb_topic': '/drone/color/image_raw',
             'depth_topic': '/drone/depth/image_raw',
             'camera_info_topic': '/drone/color/camera_info',
+            'namespace': 'drone',
+            'odom_topic': '/drone/odom',
             'frame_id': 'drone_link',
             'odom_frame_id': 'drone/odom',
             'map_frame_id': 'drone/map',
-            'namespace': 'drone',
             'approx_sync': 'true',
-            'approx_sync_max_interval': '0.1',
+            'approx_sync_max_interval': '0.2',
             'rgb_image_transport': 'compressed',
             'depth_image_transport': 'compressedDepth',
             'qos': '1',
@@ -57,7 +60,7 @@ def generate_launch_description():
             'args': '--delete_db_on_start --Vis/MaxFeatures 600 --database_path /tmp/drone_rtabmap.db',
             'rtabmap_viz': 'false',
             'Grid/3D': 'true',
-            'publish_tf_odom': 'false',
+            'publish_tf_odom': 'true',
         }.items()
     )
 
@@ -67,12 +70,13 @@ def generate_launch_description():
             'rgb_topic': '/rover/color/image_raw',
             'depth_topic': '/rover/depth/image_raw',
             'camera_info_topic': '/rover/color/camera_info',
+            'namespace': 'rover',
+            'odom_topic': '/rover/odom',
             'frame_id': 'rover_link',
             'odom_frame_id': 'rover/odom',
             'map_frame_id': 'rover/map',
-            'namespace': 'rover',
             'approx_sync': 'true',
-            'approx_sync_max_interval': '0.1',
+            'approx_sync_max_interval': '0.2',
             'rgb_image_transport': 'compressed',
             'depth_image_transport': 'compressedDepth',
             'qos': '1',
@@ -81,20 +85,30 @@ def generate_launch_description():
             'args': '--delete_db_on_start --Vis/MaxFeatures 600 --database_path /tmp/rover_rtabmap.db',
             'rtabmap_viz': 'false',
             'Grid/3D': 'true',
-            'publish_tf_odom': 'false',
+            'publish_tf_odom': 'true',
         }.items()
     )
 
-    rviz_config_dir = os.path.join(
-        get_package_share_directory('pacj'),
-        'rviz',
-        'drone_config.rviz')
+    # --- RVIZ CONFIGS ---
+    pkg_share = get_package_share_directory('pacj')
 
-    rviz_node = Node(
+    drone_rviz_config = os.path.join(pkg_share, 'rviz', 'drone_config.rviz')
+    rover_rviz_config = os.path.join(pkg_share, 'rviz', 'rover_config.rviz')
+
+    # --- TWO RVIZ INSTANCES ---
+    rviz_drone = Node(
         package='rviz2',
         executable='rviz2',
-        name='rviz2',
-        arguments=['-d', rviz_config_dir],
+        name='rviz2_drone',
+        arguments=['-d', drone_rviz_config],
+        output='screen'
+    )
+
+    rviz_rover = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2_rover',
+        arguments=['-d', rover_rviz_config],
         output='screen'
     )
 
@@ -103,7 +117,8 @@ def generate_launch_description():
         # tf_broadcaster,
         # interactive_setpoint,
         # drone_planner,
-        drone_slam,
+        # drone_slam,
         rover_slam,
-        rviz_node
+        rviz_drone,
+        rviz_rover
     ])
