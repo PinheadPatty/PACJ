628f74fbff11f6fc748933c8efa4e53f7afcb08a
Patrick Lucas <pllucas@andrew.cmu.edu>
2026-04-20 18:32:31 -0400

    removing drone_driver

--- pacj_ws/src/pacj/setup.py
+++ pacj_ws/src/pacj/setup.py
@@ -31,7 +31,6 @@ setup(
         'console_scripts': [
             'decompressor = pacj.decompress:main',
             'rover_driver = pacj.rover_driver:main',
-            'drone_driver = pacj.drone_driver:main',
             'battery = pacj.battery:main',
             'interactive_setpoint = pacj.interactive_setpoint:main',
             'drone_planner = pacj.drone_planner:main',
